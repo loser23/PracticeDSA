58c9f9f1b58f4a286d29b76f2ef38142208f3691
loser23 <68072762+loser23@users.noreply.github.com>
2020-10-02 17:24:31 +0530

    Update QueueReversal.java
    added more information regarding solution

--- language-java/Queue/solutions/QueueReversal.java
+++ language-java/Queue/solutions/QueueReversal.java
@@ -1,4 +1,5 @@
  public Queue<Integer> rev(Queue<Integer> q){
+       //we will use stack to reverse , arraylist can also be used instad of stack
         Stack<Integer> st = new Stack<>();
         while(!q.isEmpty())
             st.push(q.poll());
